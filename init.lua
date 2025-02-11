4c75a6c55069077721d398026f7bf95700682657
Navraj Sikand <nonunavraj@gmail.com>
2025-02-11 11:37:37 -0700

    Re-added obsidian config

--- init.lua
+++ init.lua
@@ -281,6 +281,44 @@ require('lazy').setup({
     end,
   },
   {
+    'epwalsh/obsidian.nvim',
+    version = '*', -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = 'markdown',
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    dependencies = {
+      -- Required.
+      'nvim-lua/plenary.nvim',
+
+      -- see below for full list of optional dependencies 👇
+    },
+    config = function()
+      vim.opt.conceallevel = 1
+      vim.keymap.set('n', 'gf', function()
+        if require('obsidian').util.cursor_on_markdown_link() then
+          return '<cmd>ObsidianFollowLink<CR>'
+        else
+          return 'gf'
+        end
+      end, { noremap = false, expr = true })
+    end,
+    opts = {
+      workspaces = {
+        {
+          name = 'personal',
+          path = '~/ASU Dropbox/Navraj Sikand/Obsidian-vaults/Personal Graph',
+        },
+      },
+    },
+  },
+  {
     'projekt0n/github-nvim-theme',
     name = 'github-theme',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
