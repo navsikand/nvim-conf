be87bcd9e0ed47b36577b2ae3902cc9dcb31370b
Navraj Sikand <nonunavraj@gmail.com>
2025-02-14 11:50:48 -0700

    Clippy works in rust

--- init.lua
+++ init.lua
@@ -774,8 +774,35 @@ require('lazy').setup({
           },
         },
         rust_analyzer = {
-          checkOnSave = {
-            command = 'clippy',
+          settings = {
+            ['rust-analyzer'] = {
+              cargo = {
+                allFeatures = true,
+                loadOutDirsFromCheck = true,
+                runBuildScripts = true,
+              },
+              -- Add clippy lints for Rust.
+              checkOnSave = {
+                allFeatures = true,
+                command = 'clippy',
+                extraArgs = {
+                  '--',
+                  '--no-deps',
+                  '-Dclippy::correctness',
+                  '-Dclippy::complexity',
+                  '-Wclippy::perf',
+                  '-Wclippy::pedantic',
+                },
+              },
+              procMacro = {
+                enable = true,
+                ignored = {
+                  ['async-trait'] = { 'async_trait' },
+                  ['napi-derive'] = { 'napi' },
+                  ['async-recursion'] = { 'async_recursion' },
+                },
+              },
+            },
           },
         },
       }
