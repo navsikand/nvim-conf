f2146a8c94d59e7d5d6ecba95dff1ffbe1601c41
Navraj Sikand <nonunavraj@gmail.com>
2025-03-27 18:45:02 -0700

    file path

--- init.lua
+++ init.lua
@@ -1163,6 +1163,7 @@ require('lazy').setup({
         sections = {
           lualine_a = { 'mode' },
           lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { { 'filename', path = 1 } }, -- Relative file path displayed here.
           lualine_x = {
             'encoding',
             'filetype',
